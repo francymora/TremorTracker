ba5b2e20d42a0789b0d8ace97a8e9439eb6576bc
francymora <79939741+francymora@users.noreply.github.com>
2024-03-27 09:03:03 +0000

    Initial Commit

---
+++ TremorTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TremorTracker
+//
+//  Created by Francesco Morandi on 27/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TremorTracker/TremorTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TremorTrackerApp.swift
+//  TremorTracker
+//
+//  Created by Francesco Morandi on 27/03/24.
+//
+
+import SwiftUI
+
+@main
+struct TremorTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
